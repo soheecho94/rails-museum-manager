75692f5e2c14859721e22c6de6f99ea1016ea947
soheecho94 <soheecho94@gmail.com>
2017-09-16 11:55:09 +0900

    add artwork form

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,19 @@
 class ArtworksController < ApplicationController
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def artwork_params
+      params.require(:artwork).permit(:title, :artist, :year, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   resources :categories
   resources :artworks
-  resources :museums
+  resources :museums, only: [:new, :create, :edit, :update]
+  resources :museums, only: [:show] do
+    resources :artworks
+  end
+
   resources :users
   resources :locations, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
