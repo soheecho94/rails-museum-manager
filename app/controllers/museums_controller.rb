1d53098ad89ece2ccd2f3a4428fe8ce28949dec0
soheecho94 <soheecho94@gmail.com>
2017-09-16 23:26:57 +0900

    add edit form for museum

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -16,6 +16,19 @@ class MuseumsController < ApplicationController
     @museum = Museum.find(params[:id])
   end
 
+  def edit
+    @museum = Museum.find(params[:id])
+  end
+
+  def update
+    @museum = Museum.find(params[:id])
+    if @museum.update(museum_params)
+      redirect_to museum_path(@museum)
+    else
+      render :edit
+    end
+  end
+
   private
     def museum_params
       params.require(:museum).permit(:name, :description, :location_name, :location_id)
