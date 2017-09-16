e75483a40fa37b84b3d29f6bc078d86c36b9ddec
soheecho94 <soheecho94@gmail.com>
2017-09-16 14:11:00 +0900

    nest artwork - museum

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,19 +1,27 @@
 class ArtworksController < ApplicationController
   def new
-    @artwork = Artwork.new
+    @artwork = Artwork.new(museum_id: params[:museum_id])
   end
 
   def create
+    @museum = Museum.find_by(id: params[:artwork][:museum_id])
     @artwork = Artwork.new(artwork_params)
     if @artwork.save
-      redirect_to root_path
+      redirect_to museum_artwork_path(@museum, @artwork)
     else
       render :new
     end
   end
 
+  def show
+    if params[:museum_id]
+      @museum = Museum.find_by(id: params[:museum_id])
+      @artwork = @museum.artworks.find_by(id: params[:id])
+    end
+  end
+
   private
     def artwork_params
-      params.require(:artwork).permit(:title, :artist, :year, :description, :museum_name, :museum_id)
+      params.require(:artwork).permit(:title, :artist, :year, :description, :museum_id)
     end
 end
