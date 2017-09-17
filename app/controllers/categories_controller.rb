4397473bd37428b115d535ba4b29e337b53ab361
soheecho94 <soheecho94@gmail.com>
2017-09-17 13:49:25 +0900

    add category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,10 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   private
     def category_params
       params.require(:category).permit(:name)
