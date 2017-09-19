50012976f57d5c485611350439870dae987766e3
soheecho94 <soheecho94@gmail.com>
2017-09-19 19:52:12 +0900

    dry museums controller

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,4 +1,6 @@
 class MuseumsController < ApplicationController
+  before_action :museum_var, only: [:show, :edit, :update, :destroy]
+
   def new
     @museum = Museum.new
   end
@@ -13,15 +15,12 @@ class MuseumsController < ApplicationController
   end
 
   def show
-    @museum = Museum.find(params[:id])
   end
 
   def edit
-    @museum = Museum.find(params[:id])
   end
 
   def update
-    @museum = Museum.find(params[:id])
     if @museum.update(museum_params)
       redirect_to museum_path(@museum)
     else
@@ -34,7 +33,6 @@ class MuseumsController < ApplicationController
   end
 
   def destroy
-    @museum = Museum.find(params[:id])
     @museum.destroy
     redirect_to root_path
   end
@@ -42,5 +40,9 @@ class MuseumsController < ApplicationController
   private
     def museum_params
       params.require(:museum).permit(:name, :description, :location_id, location_attributes: [:name])
+    end
+
+    def museum_var
+      @museum = Museum.find(params[:id])
     end
 end
