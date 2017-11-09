b8b572cfb99f908511ec17ac2d7fc7a1327c0e77
soheecho94 <soheecho94@gmail.com>
2017-11-09 01:24:41 -0500

    modify render() and show page container

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,11 +1,17 @@
 class Artwork {
-  constructor(title, description) {
+  constructor(title, year, artist, id, museum_id) {
     this.title = title
-    this.description = description
+    this.year = year
+    this.artist = artist
+    this.id = id
+    this.museum_id = museum_id
   }
+
   render() {
-    console.log(this.title)
-    return "hello"
+    let li = document.createElement('li')
+    console.log(this.museum_id)
+    li.innerHTML = `<a href="/museums/${this.museum_id}/artworks/${this.id}">` + `${this.title} | ${this.artist} | ${this.year}` + "</a>"
+    return li.innerHTML
   }
 }
 
@@ -16,7 +22,7 @@ $(document).ready(function() {
 function renderArtworks() {
   var id = $(".artwork_list").attr("id")
   $.get(`/categories/${id}/artworks`, function(data) {
-    var html = new Artwork(data[0].title, data[0].description).render()
-    $(".artwork_list").append(html)
+    var html = new Artwork(data[0].title, data[0].year, data[0].artist, data[0].id, data[0].museum.id).render()
+    $(".artwork_list ul").append(html)
   })
 }
