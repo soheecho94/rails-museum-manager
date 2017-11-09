77b7036da67481ca04307afbc20b10b4b3efcc3e
soheecho94 <soheecho94@gmail.com>
2017-11-09 18:02:48 -0500

    replace params in url

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -6,7 +6,7 @@ function renderComments() {
   var idInfo = document.getElementsByClassName('comments')[0]
   var artworkId = idInfo.id
   var museumId = idInfo.dataset.id
-  $.get('/museums/1/artworks/1.json', function(artwork) {
+  $.get(`/museums/${museumId}/artworks/${artworkId}.json`, function(artwork) {
     var comments = artwork.user_comments
     comments.forEach(function(comment) {
       $(".comments ul").append("<li>" + comment.comments + "</li>")
