83fca98391d183a79fa1ad8dcd2e8397d6f2f55f
soheecho94 <soheecho94@gmail.com>
2017-12-03 09:28:24 +0900

    add categories serializer, add categories to next artwork and comment out comments

--- app/assets/javascripts/artwork.js
+++ app/assets/javascripts/artwork.js
@@ -10,13 +10,43 @@ function next() {
       var artworkId = parseInt($(".js-next").attr("data-id"));
       var nextArtworkIndex = artworks.findIndex(artwork => artwork.id === artworkId) + 1;
       var nextArtworkData = artworks[nextArtworkIndex]
+      var categories = nextArtworkData.categories
+      var comments = nextArtworkData.user_comments
       $(".artwork_show h2").text(nextArtworkData.title)
       $(".artwork_show p#artist").html("<strong>ARTIST: </strong>" + nextArtworkData.artist)
       $(".artwork_show p#year").html("<strong>YEAR CREATED: </strong>" + nextArtworkData.year)
       $(".artwork_show p#description").html("<strong>DESCRIPTION: </strong>" + nextArtworkData.description)
-      $(".comments").html("")
-      debugger
+
+      categories.forEach(function(category) {
+        $(".categories_list").html(`<strong>CATEGORIES: </strong><a href="/categories/${category.id}">${category.name}</a>` + " | ")
+      })
+
+      // getCurrentUser();
+      // comments.forEach(function(comment) {
+      //   if (current_user && current_user.username === comment.user.username && parseInt(artworkId) === comment.artwork.id) {
+      //     $(".comments ul").append("<li>" + comment.user.username + ": " + comment.comments + "</li>" + `<a href="/artworks/${artworkId}/user_comments/${comment.id}/edit">` + "Edit" + "</a>")
+      //   } else if (parseInt(artworkId) !== comment.artwork.id) {
+      //     $(".comments ul").append("")
+      //   } else if (current_user && current_user.username !== comment.user.username && parseInt(artworkId) === comment.artwork.id) {
+      //     $(".comments ul").append("<li>" + comment.user.username + ": " + comment.comments + "</li>")
+      //   }
+      // })
+
       $(".new_user_comment").attr('action', `/artworks/${nextArtworkData.id}/user_comments`)
     })
   })
 }
+
+// function renderCommentsOnNext() {
+//   getCurrentUser();
+//   var comments = nextArtworkData.user_comments
+//     comments.forEach(function(comment) {
+//       if (current_user && current_user.username === comment.user.username && parseInt(artworkId) === comment.artwork.id) {
+//         $(".comments ul").append("<li>" + comment.user.username + ": " + comment.comments + "</li>" + `<a href="/artworks/${artworkId}/user_comments/${comment.id}/edit">` + "Edit" + "</a>")
+//       } else if (parseInt(artworkId) !== comment.artwork.id) {
+//         $(".comments ul").append("")
+//       } else if (current_user && current_user.username !== comment.user.username && parseInt(artworkId) === comment.artwork.id) {
+//         $(".comments ul").append("<li>" + comment.user.username + ": " + comment.comments + "</li>")
+//       }
+//     })
+// }
--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -2,4 +2,5 @@ class ArtworkSerializer < ActiveModel::Serializer
   attributes :id, :title, :artist, :year, :description
   belongs_to :museum
   has_many :user_comments
+  has_many :categories
 end
