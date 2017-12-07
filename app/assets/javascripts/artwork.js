4ee423ea13ab78af02a10c54ffe132dab1801da5
soheecho94 <soheecho94@gmail.com>
2017-12-07 23:01:47 +0900

    refactor with Luke

--- app/assets/javascripts/artwork.js
+++ app/assets/javascripts/artwork.js
@@ -1,42 +1,59 @@
 $(document).on("turbolinks:load", function() {
+  store.state.artworkIds = $('#artwork-ids').data("artwork-ids");
+  store.state.currentArtworkId = $(".js-next").data("id");
+  store.state.currentUser = $('#current-user').data("current-user");
+console.log(store.state)
   next();
 })
+
+var store = {
+  state: {
+    artworkIds: [],
+    currentArtworkId: undefined,
+    artworks: [],
+    currentUser: undefined
+  }
+}
+
+function getNextArtworkId() {
+  const { artworkIds, currentArtworkId } = store.state
+  var index = artworkIds.findIndex(id => id === currentArtworkId)
+  if (index === artworkIds.length - 1) {
+    return artworkIds[0]
+  }
+  return artworkIds[index + 1]
+}
 
 function next() {
   $(".js-next").on("click", function(event) {
     event.preventDefault();
-    var museumId = parseInt($(".js-next").attr("data-museum"));
-    $.get("/museums/" + museumId + "/artworks", function(artworks) {
-      var artworkId = parseInt($(".js-next").attr("data-id"));
-      var nextArtworkIndex = artworks.findIndex(artwork => artwork.id === artworkId) + 1;
-      var nextArtworkData = artworks[nextArtworkIndex]
-      var nextArtworkId = nextArtworkData.id
-      var categories = nextArtworkData.categories
-      var comments = nextArtworkData.user_comments
+    const nextArtworkId = getNextArtworkId()
+    const museumId = parseInt($(".js-next").attr("data-museum"));
+    $.get("/museums/" + museumId + "/artworks/" + nextArtworkId + ".json", function(artworkData) {
+      store.state.currentArtworkId = artworkData.id
+      $(".artwork_show h2").text(artworkData.title)
+      $(".artwork_show p#artist").html("<strong>ARTIST: </strong>" + artworkData.artist)
+      $(".artwork_show p#year").html("<strong>YEAR CREATED: </strong>" + artworkData.year)
+      $(".artwork_show p#description").html("<strong>DESCRIPTION: </strong>" + artworkData.description)
 
-      $(".artwork_show h2").text(nextArtworkData.title)
-      $(".artwork_show p#artist").html("<strong>ARTIST: </strong>" + nextArtworkData.artist)
-      $(".artwork_show p#year").html("<strong>YEAR CREATED: </strong>" + nextArtworkData.year)
-      $(".artwork_show p#description").html("<strong>DESCRIPTION: </strong>" + nextArtworkData.description)
-
-      categories.forEach(function(category) {
+      artworkData.categories.forEach(function(category) {
         $(".categories_list").html(`<strong>CATEGORIES: </strong><a href="/categories/${category.id}">${category.name}</a>` + " | ")
       })
 
-      getCurrentUser();
+      const { currentUser } = store.state
       $(".comments ul").text("")
-      comments.forEach(function(comment) {
-        if (current_user && current_user.username === comment.username && parseInt(nextArtworkId) === comment.artwork_id) {
-          $(".comments ul").append("<li>" + comment.username + ": " + comment.comments + "</li>" + `<a href="/artworks/${artworkId}/user_comments/${comment.id}/edit">` + "Edit" + "</a>")
+      artworkData.user_comments.forEach(function(comment) {
+        if (currentUser && currentUser.id === comment.user_id && parseInt(nextArtworkId) === comment.artwork_id) {
+          $(".comments ul").append("<li>" + comment.username + ": " + comment.comments + "</li>" + `<a href="/artworks/${artworkData.id}/user_comments/${comment.id}/edit">` + "Edit" + "</a>")
         } else if (parseInt(nextArtworkId) !== comment.artwork_id) {
           $(".comments ul").append("")
-        } else if (current_user && current_user.username !== comment.username && parseInt(nextArtworkId) === comment.artwork_id) {
+        } else if (currentUser && currentUser.id !== comment.user_id && parseInt(nextArtworkId) === comment.artwork_id) {
           $(".comments ul").append("<li>" + comment.username + ": " + comment.comments + "</li>")
         }
       })
 
-      $(".button_to").attr('action', `/user_artworks?artwork_id=${nextArtworkData.id}`)
-      $(".new_user_comment").attr('action', `/artworks/${nextArtworkData.id}/user_comments`)
+      $(".button_to").attr('action', `/user_artworks?artwork_id=${artworkData.id}`)
+      $(".new_user_comment").attr('action', `/artworks/${artworkData.id}/user_comments`)
     })
   })
 }
